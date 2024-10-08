daf61f148725e8aac1157adb22fa310e5c1a3d96
Андрій Сороковський <andrijsorokovskij@MacBook-Air-Andrij.local>
2024-10-08 14:57:14 +0300

    start

---
+++ Task 1/MyList.cs
@@ -0,0 +1,7 @@
+namespace Task_1
+{
+    public class MyList
+    {
+        
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Task_1;
+public static class Program
+{
+    public static void Main()
+    {
+
+    }
+}
